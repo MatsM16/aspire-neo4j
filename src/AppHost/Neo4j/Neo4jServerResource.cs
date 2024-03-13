c93be7e78c28633fe1e3533a24a20f00c2afecfa
MatsM16 <matdyr23@gmail.com>
2024-03-13 21:52:57 +0100

    Added Neo4j container stuff

---
+++ src/AppHost/Neo4j/Neo4jServerResource.cs
@@ -0,0 +1,34 @@
+﻿namespace AppHost.Neo4j;
+/*
+public sealed class Neo4jServerResource : ContainerResource
+{
+    internal const string PrimaryEndpointName = "bolt";
+    internal const string DashboardEndpointName = "http";
+
+    public Neo4jServerResource(string name) : base(name)
+    {
+        PrimaryEndpoint = new(this, PrimaryEndpointName);
+        DashboardEndpoint = new(this, DashboardEndpointName);
+
+        PasswordInput = new(this, "password");
+    }
+
+    public EndpointReference PrimaryEndpoint { get; }
+
+    public EndpointReference DashboardEndpoint { get; }
+
+    internal InputReference PasswordInput { get; }
+
+    private ReferenceExpression ConnectionString =>
+        ReferenceExpression.Create(
+            $"bolt://neo4j:{PasswordInput}@{PrimaryEndpoint.Property(EndpointProperty.Host)}:{PrimaryEndpoint.Property(EndpointProperty.Port)}");
+
+    public string Username => "neo4j";
+    public string Password => "neo4j";
+    //public string Password => PasswordInput.Input.Value ?? throw new InvalidOperationException("Password cannot be null.");
+
+    public ValueTask<string?> GetConnectionStringAsync(CancellationToken cancellationToken = default) => ConnectionString.GetValueAsync(cancellationToken);
+
+    public string? ConnectionStringExpression => ConnectionString.ValueExpression;
+}
+/**/
